d6d4edf6dbe54cdcd73569344e457987820fa5b2
Estebandavila98 <104226810+Estebandavila98@users.noreply.github.com>
2023-11-10 16:05:36 -0300

    Update PRE TDATablero.cpp

--- TDAs/PRE TDATablero.cpp
+++ TDAs/PRE TDATablero.cpp
@@ -1,27 +1,174 @@
 #include <iostream>
 #include "Lista.h"
+/*
+#include "Espia.h"
+#include "Mina.h"
+#include "Tesoro.h"
+*/
 
 using namespace std;
 
-//pre celda de prueba
+enum EstadoCelda{
+	INACTIVO,
+	ACTIVO,
+	BLINDADA
+};
+
+
+
+/*
+* Una Celda almacena objetos e información necesaria para el juego tesoro binario.
+*/
 class Celda {
 
     private:
 
-        unsigned int contenidoP;
+        EstadoCelda estado;
+		//Espia* espia;
+		//Mina* mina;
+		//Lista<Tesoro*>* Tesoros;
+		unsigned int turnosInactivosRestantes;
+		unsigned int turnosBlindajeRestante;
+		unsigned int coordenadaX; //no serian necesarias las dejamos por si acaso
+		unsigned int coordenadaY;
+		unsigned int coordenadaZ;
 
     public:
 
-        Celda(unsigned int contenidoP){
-			this->contenidoP = contenidoP;
+		/*
+		* Pre: Coordenadas X Y Z son numeros mayor a cero.
+		* Post: Inicializa una celda con valores default.
+		*/
+        Celda(unsigned int coordenadaX, unsigned int coordenadaY, unsigned int coordenadaZ){
+			this->coordenadaX = coordenadaX;
+			this->coordenadaY = coordenadaY;
+			this->coordenadaZ = coordenadaZ;
+			this->estado = ACTIVO;
+			this->turnosInactivosRestantes = 0;
+			this->turnosBlindajeRestante = 0;
+			//this->Tesoros = new Lista<Tesoro*>;
+			//this->Espia = NULL;
+			//this->Mina = NULL;
 		}
 
-		void setContenido(unsigned int contenidoP){
-			this->contenidoP = contenidoP;
+		/*
+		* Pre: -
+		* Post: Devuelve la coordenada X de la celda.
+		*/
+		unsigned int getCoordenadaX(){
+			return this->coordenadaX;
 		}
 
-		unsigned int getContenido(){
-			return this->contenidoP;
+
+		/*
+		* Pre: -
+		* Post: Devuelve la coordenada Y de la celda.
+		*/
+		unsigned int getCoordenadaY(){
+			return this->coordenadaY;
+		}
+
+
+		/*
+		* Pre: -
+		* Post: Devuelve la coordenada Z de la celda.
+		*/
+		unsigned int getCoordenadaZ(){
+			return this->coordenadaZ;
+		}
+
+		/*
+		* Pre: -
+		* Post: Devuelve el estado de la celda
+		*/
+		EstadoCelda getEstado(){
+			return this->estado;
+		}
+
+		/*
+		* Pre: -
+		* Post: Devuelve turnosInactivosRestantes de la celda.
+		*/
+		unsigned int getTurnosInactivosRestantes(){
+			return this->turnosInactivosRestantes;
+		}
+
+		/*
+		* Pre: -
+		* Post: Devuelve turnosBlindajeRestante de la celda.
+		*/
+		unsigned int getTurnosBlindajeRestante(){
+			return this->turnosBlindajeRestante;
+		}
+
+		/*
+		* Pre: -
+		* Post: Devuelve puntero al unico espía de la celda.
+		*/
+		/*
+		Espia* obtenerEspia(){
+			return this->Espia; 
+		}*/
+
+		/*
+		* Pre: -
+		* Post: Devuelve puntero a la unica mina de la celda.
+		*/
+		/*
+		Mina* obtenerMina(){
+			return this->Mina; 
+		}*/
+
+		/*
+		* Pre: -
+		* Post: Devuelve puntero a la unica mina de la celda.
+		*/
+		/*
+		Lista<Tesoro*>* obtenerTesoros(){
+			return this->Tesoros; 
+		}*/
+
+		/*
+		* Pre: -
+		* Post: Modifica el estado de la celda
+		*/
+		void setEstado(EstadoCelda nuevoEstado ){
+			this-> estado = nuevoEstado;
+		}
+
+
+		/*
+		* Pre: -
+		* Post: Asignar una cantidad de turnos inactivos a la celda
+		*/
+		void setTurnosInactivosRestantes(unsigned int cantidadTurnosInactivos){
+			this->turnosInactivosRestantes = cantidadTurnosInactivos;
+		}
+
+
+		/*
+		* Pre: -
+		* Post: Asignar una cantidad de turnos de blindaje a la celda
+		*/
+		void setTurnosBlindajeRestantes(unsigned short cantidadTurnosBlindaje){
+			this -> turnosBlindajeRestante = cantidadTurnosBlindaje;
+		}
+		
+		/*
+		* Pre: -
+		* Post: Libera datos dinamicos de la celda
+		*/
+		~Celda(){
+			/*
+			delete espia;
+			delete mina;
+			Lista<Tesoro*>* ptrlistptrtesoro = this->Tesoros;
+			ptrlistptrtesoro->iniciarCursor()
+			while (ptrlistptrtesoro->avanzarCursor()){
+				delete ptrlistptrtesoro->obtenerCursor();
+			}
+			delete Tesoros;
+			*/
 		}
 };
 
@@ -50,7 +197,7 @@ class Tablero {
 				for (unsigned int j = 1; j <= dimensionY; j++){
 					Lista<Celda*>*  ptrlistdeptrcelda = new Lista<Celda*>;
 					for (unsigned int i = 1; i <= dimensionX; i++){
-						Celda* ptrcelda = new Celda(i+j+k);
+						Celda* ptrcelda = new Celda(i,j,k);
 						ptrlistdeptrcelda->agregar(ptrcelda);
 					}
 					ptrlistptrlistdeptrcelda->agregar(ptrlistdeptrcelda);
@@ -113,7 +260,7 @@ class Tablero {
 					Lista<Celda*>* ptrlistdeptrcelda = ptrlistptrlistdeptrcelda->obtenerCursor();
 					ptrlistdeptrcelda->iniciarCursor();
 					while (ptrlistdeptrcelda->avanzarCursor()){
-						cout <<  ptrlistdeptrcelda->obtenerCursor()->getContenido() << " ";
+						cout <<  ptrlistdeptrcelda->obtenerCursor()->getCoordenadaX() << ptrlistdeptrcelda->obtenerCursor()->getCoordenadaY() << " ";
 					}
 					cout << endl;
 				}
@@ -148,9 +295,11 @@ int main() {
 	unsigned int dimensionX = 4, dimensionY = 3, dimensionZ = 2;
 	Tablero* ptrtablerodeprueba = new Tablero(dimensionX, dimensionY, dimensionZ);
 	ptrtablerodeprueba->mostrarTablero();
-	cout << ptrtablerodeprueba->obtenerPosicion(4,2,1)->getContenido() << endl;
-	ptrtablerodeprueba->obtenerPosicion(4,2,1)->setContenido(20); 
+	cout << ptrtablerodeprueba->obtenerPosicion(4,2,1)->getEstado() << endl;
+	ptrtablerodeprueba->obtenerPosicion(4,2,1)->setEstado(INACTIVO);
+	cout << ptrtablerodeprueba->obtenerPosicion(4,2,1)->getEstado() << endl;
 	ptrtablerodeprueba->mostrarTablero();
 	delete ptrtablerodeprueba;
 	return 0;
 }
+
