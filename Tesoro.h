e807902c7260c1253bf91f14153dd9e1684a533e
Bruno Paladino <126134408+BrunoPaladino@users.noreply.github.com>
2023-11-10 17:45:47 -0300

    Update Tesoro.h

--- Tesoro.h
+++ Tesoro.h
@@ -17,37 +17,57 @@ public:
 	 * Pre: -
 	 * Post: Inicializa Tesoro indicando el jugador propietario del mismo y las coordenadas del Tesoro
 	 */
-    Tesoro(Jugador*, unsigned int x, unsigned int y, unsigned int z);
+    Tesoro(Jugador*, unsigned int x, unsigned int y, unsigned int z)
+    {
+        this-> propietario = new Jugador*;
+        this-> coordenadaCeldaContenedoraX = x;
+        this-> coordenadaCeldaContenedoraY = y;
+        this-> coordenadaCeldaContenedoraZ = z;
+    };
 
 	/*
 	 * Pre: -
 	 * Post: Devuelve el jugador propietario del tesoro
 	 */
-    Jugador* getPropietario();
+    Jugador* getPropietario()
+    {
+        this-> propietario;
+    }
 
 	/*
 	 * Pre: -
 	 * Post: Devuelve la coordenada X de la celda del jugador actual
 	 */
-    unsigned int getCeldaContenedoraX();
+    unsigned int getCeldaContenedoraX()
+    {
+        this->coordenadaCeldaContenedoraX;
+    }
 
 	/*
 	 * Pre: -
 	 * Post: Devuelve la coordenada Y de la celda del jugador actual
 	 */
-    unsigned int getCeldaContenedoraY();
+    unsigned int getCeldaContenedoraY()
+    {
+        this->coordenadaCeldaContenedoraY;
+    }
 
     /*
 	 * Pre: -
 	 * Post: Devuelve la coordenada Z de la celda del jugador actual
 	 */
-    unsigned int getCeldaContenedoraZ();
-
+    unsigned int getCeldaContenedoraZ()
+    {
+        this->coordenadaCeldaContenedoraZ;
+    }
 	/*
 	 * Pre: -
 	 * Post: Elimina el Tesoro
 	 */
-    ~Tesoro();
+    ~Tesoro()
+    {
+        delete propietario;
+    };
 };
 
 
