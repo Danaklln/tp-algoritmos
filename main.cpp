65065ae162898914b48863481a4d991c74cb3f36
GustavoScilingo <gscilingo@fi.uba.ar>
2023-11-29 20:19:03 -0300

    Agregadas dos funciones para testear ExportadorTablero, movido viejo main a main.cpp.old

--- main.cpp
+++ main.cpp
@@ -1,28 +1,23 @@
-#include "./TDAs/TesoroBinario.h"
-#include "./TDAs/Interfaz.h"
+/*
+*           ESTE ES UN MAIN.CPP DE PRUEBA.
+*   EL MAIN.CPP ANTERIOR SE RENOMBRO A MAIN.CPP.OLD
+*/
+#include "./TDAs/Jugador.h"
+#include "./TDAs/ExportadorTablero.h"
+#include "./TDAs/Tablero.h"
 
-int main(){
-    
-    unsigned int cantidadJugadores;
-    unsigned int x,y,z;
-    TesoroBinario * juego;
-    Interfaz * interfaz;
-    bool salir;
+int main()
+{
+    Jugador* jugadorUno = new Jugador(1);
+    Tablero* tablero = new Tablero(3, 3, 3, jugadorUno);
+    ExportadorTablero* exportadorTablero = new ExportadorTablero(tablero);
 
-    interfaz = new Interfaz;
+    // Agregando objetos al tablero con jugadorUno de propietario en el segundo piso
+    tablero->agregarNuevoEspia(jugadorUno, 1, 1, 2);
+    tablero->agregarNuevoEspia(jugadorUno, 1, 2, 2);
+    tablero->agregarNuevoEspia(jugadorUno, 1, 3, 2);
+    tablero->agregarNuevoTesoro(jugadorUno, 1, 1, 2);
 
-    cantidadJugadores = interfaz->pedirCantidadJugadores();
-    interfaz->pedirDimensiones(&x,&y,&z);
-    juego = new TesoroBinario(cantidadJugadores,x,y,z,interfaz);
-    
-    salir = juego->iniciarJuego();
-    if(salir){
-        interfaz->msjSalida();
-    }
-
-    delete juego;
-    delete interfaz;
-
-    return 0;
+    // Exportando el piso dos del tablero
+    exportadorTablero->exportarPisoTablero(tablero, jugadorUno, 2);
 }
-
