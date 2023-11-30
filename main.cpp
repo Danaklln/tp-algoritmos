e684f7198eeee70564fcc83e310c5d1857f33223
GustavoScilingo <gscilingo@fi.uba.ar>
2023-11-29 22:26:33 -0300

    Solucionado error por el cual un tesoro rival revelado rodeado de tesoros propios hacia que no se viera el tesoro rival

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 #include "./TDAs/Jugador.h"
 #include "./TDAs/ExportadorTablero.h"
 #include "./TDAs/Tablero.h"
+#include "./TDAs/Tesoro.h"
 
 int main()
 {
     Jugador* jugadorUno = new Jugador(1);
+    Jugador* jugadorDos = new Jugador(2);
     Tablero* tablero = new Tablero(3, 3, 3, jugadorUno);
     ExportadorTablero* exportadorTablero = new ExportadorTablero(tablero);
 
@@ -17,6 +19,7 @@ int main()
     std::cout << tablero->agregarNuevoEspia(jugadorUno, 1, 2, 2);
     std::cout << tablero->agregarNuevoEspia(jugadorUno, 1, 3, 2);
     tablero->agregarNuevoTesoro(jugadorUno, 1, 2, 2);
+    tablero->agregarNuevoTesoro(jugadorDos, 1, 1, 2);
 
     // Exportando el piso dos del tablero
     exportadorTablero->exportarPisoTablero(tablero, jugadorUno, 2);
