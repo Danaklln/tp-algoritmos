398281acbf10fa34e85d26bb22403b47b708a921
GustavoScilingo <gscilingo@fi.uba.ar>
2023-11-29 23:01:25 -0300

    .

--- main.cpp
+++ main.cpp
@@ -6,23 +6,22 @@
 #include "./TDAs/ExportadorTablero.h"
 #include "./TDAs/Tablero.h"
 #include "./TDAs/Tesoro.h"
+#include "TDAs/Mina.h"
 
 int main()
 {
     Jugador* jugadorUno = new Jugador(1);
     Jugador* jugadorDos = new Jugador(2);
-    Tablero* tablero = new Tablero(3, 5, 3, jugadorUno);
+    Tablero* tablero = new Tablero(3, 4, 3, jugadorUno);
     ExportadorTablero* exportadorTablero = new ExportadorTablero(tablero);
 
     // Agregando objetos al tablero con jugadorUno de propietario en el segundo piso
     tablero->agregarNuevoEspia(jugadorUno, 1, 1, 1);
-    tablero->agregarNuevoEspia(jugadorUno, 1, 2, 1);
-    tablero->agregarNuevoEspia(jugadorUno, 1, 3, 1);
     tablero->agregarNuevoEspia(jugadorUno, 1, 4, 1);
-    tablero->agregarNuevoEspia(jugadorUno, 1, 5, 1);
     tablero->agregarNuevoTesoro(jugadorUno, 1, 2, 1);
     tablero->agregarNuevoTesoro(jugadorDos, 1, 2, 1);
-
+    tablero->obtenerPtrCelda(1,2,1)->inactivarCelda(3);
+    tablero->obtenerPtrCelda(1,1,1)->addMina(new Mina(5, jugadorUno));
     // Exportando el piso dos del tablero
     exportadorTablero->exportarPisoTablero(tablero, jugadorUno, 1);
 }
