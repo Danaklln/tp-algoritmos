0270513f922c6e0dc19d96c92080203cdc4cb991
GustavoScilingo <gscilingo@fi.uba.ar>
2023-12-06 20:32:29 -0300

    ...

--- mainP.cpp
+++ mainP.cpp
@@ -1,17 +1,17 @@
 #include "./TDAs/Juego.h"
 #include <iostream>
+
 using namespace std;
 
 int main() {
-	unsigned int cantidadDeJugadores = 2, dimensionX = 3, dimensionY = 3, dimensionZ = 2, numeroDeTesorosPorJugador = 3;
-	cout << "Bienvenido a TesoroBinario, para empezar a jugar especifique las dimensiones del tablero (ancho largo alto): " << endl;
-    cin >> dimensionX >> dimensionY >> dimensionZ;
-    cout << "Ingrese la cantidad de jugadores" << endl;
-    cin >> cantidadDeJugadores;
-	Juego* ptrjuego = new Juego(cantidadDeJugadores, dimensionX, dimensionY, dimensionZ);
-	ptrjuego->iniciarJuego(numeroDeTesorosPorJugador);
-	ptrjuego->jugarJuego();
-	delete ptrjuego;
-	return 0;
+  unsigned int cantidadDeJugadores = 2, dimensionX = 3, dimensionY = 3, dimensionZ = 2, numeroDeTesorosPorJugador = 3;
+  cout << "Bienvenido a TesoroBinario, para empezar a jugar especifique las dimensiones del tablero (ancho largo alto): " << endl;
+  cin >> dimensionX >> dimensionY >> dimensionZ;
+  cout << "Ingrese la cantidad de jugadores" << endl;
+  cin >> cantidadDeJugadores;
+  Juego* ptrjuego = new Juego(cantidadDeJugadores, dimensionX, dimensionY, dimensionZ);
+  ptrjuego->iniciarJuego(numeroDeTesorosPorJugador);
+  ptrjuego->jugarJuego();
+  delete ptrjuego;
+  return 0;
 }
-
